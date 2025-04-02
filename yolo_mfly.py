09fe2660024b0c0d43b01ab190fb3bfcf0f353d0
Jackie Lin <jaclin@umich.edu>
2025-04-02 16:23:10 -0400

    first run with finetuning

--- yolo_mfly.py
+++ yolo_mfly.py
@@ -11,8 +11,29 @@ result[0].save(filename='result.jpg')
 '''
 model = YOLO("yolo11n.pt")
 
-model.train(data="/mnt/c/Users/jacki/Documents/mfly_yolo/suas.yaml", epochs=100,
-    imgsz=640,
-    batch=16)
+pretrained = True
+optimizer = "AdamW"
+lr0 = 0.01
+save = True
+device=0
+warmup_epochs = 3.0
+momentum = 0.937
+val = True
+save = True
+plots = True
+
+model.train(
+            data="/mnt/c/Users/jacki/Documents/mfly_yolo/suas.yaml",
+            epochs=100,
+            imgsz=640,
+            batch=16,
+            optimizer=optimizer,
+            lr0=lr0,
+            warmup_epochs=warmup_epochs,
+            momentum=momentum,
+            val=val,
+            save=save,
+            plots=plots
+            )
 
 torch.save(model.model.state_dict(), "model.pth")
