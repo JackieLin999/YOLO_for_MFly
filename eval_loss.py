0476192ccc28e09603d48b83f9739975fe70ef51
Jackie Lin <jaclin@umich.edu>
2025-04-03 13:38:23 -0400

    run_2 results and run_1 plots

---
+++ eval_loss.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load your CSV
+df = pd.read_csv("run_1/runs/detect/train14/results.csv")
+
+# Create a figure
+plt.figure(figsize=(12, 6))
+
+plt.plot(df["val/box_loss"], label="Val Box Loss", color="red", linestyle="--")
+plt.plot(df["val/cls_loss"], label="val cls_loss", color="green", linestyle="--")
+plt.plot(df["val/dfl_loss"], label="val dfl_loss", color="blue", linestyle="--")
+
+plt.plot(df["train/box_loss"], label="train Box Loss", color="red", linestyle="--")
+plt.plot(df["train/cls_loss"], label="train cls_loss", color="green", linestyle="--")
+plt.plot(df["train/dfl_loss"], label="train dfl_loss", color="blue", linestyle="--")
+
+# Add labels and title
+plt.xlabel("Epoch"), plt.ylabel("Value")
+plt.title("run_1 loss comparsion")
+plt.legend()
+
+# Save the plot instead of showing it
+plt.savefig("run_1_loss.png", dpi=300, bbox_inches='tight')
+plt.close()  # Clean up memory
---
+++ run_2.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+import os
+from datasets import load_dataset
+import torch
+
+
+model = YOLO("yolo11n.pt")
+pretrained = True
+optimizer = "AdamW"
+lr0 = 0.01
+save = True
+device=0
+warmup_epochs = 3.0
+momentum = 0.937
+val = True
+save = True
+plots = True
+box = 5.0
+
+model.train(
+            data="suas.yaml",
+            epochs=100,
+            imgsz=640,
+            batch=16,
+            optimizer=optimizer,
+            lr0=lr0,
+            warmup_epochs=warmup_epochs,
+            momentum=momentum,
+            val=val,
+            save=save,
+            plots=plots,
+            box=box
+            )
+
+torch.save(model.model.state_dict(), "model.pth")
