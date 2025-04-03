e0af2be47f74ebf5fa77d14fa0f40a375cdd6ffa
Jackie Lin <jaclin@umich.edu>
2025-04-03 13:38:23 -0400

    run_2 results and run_1 plots

---
+++ eval.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load your CSV
+df = pd.read_csv("run_1/runs/detect/train14/results.csv")
+
+# Create a figure
+plt.figure(figsize=(12, 6))
+
+# Plot training and validation metrics
+plt.plot(df["metrics/mAP50(B)"], label="mAP50", color="blue")
+plt.plot(df["metrics/mAP50-95(B)"], label="mAP50-95(B)", color="purple")
+plt.plot(df["metrics/recall(B)"], label="metrics/recall(B)", color="green")
+plt.plot(df["metrics/precision(B)"], label="precision", color="black")
+
+
+# Add labels and title
+plt.xlabel("Epoch"), plt.ylabel("Value")
+plt.title("run 1 metric")
+plt.legend()
+
+# Save the plot instead of showing it
+plt.savefig("run_1_metric.png", dpi=300, bbox_inches='tight')
+plt.close()  # Clean up memory
