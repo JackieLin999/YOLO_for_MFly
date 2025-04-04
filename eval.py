280de7fd7c5a324598911cb74a82548aa545303e
Jackie Lin <jaclin@umich.edu>
2025-04-04 15:52:56 -0400

    run 10 code and run 7 result

--- eval.py
+++ eval.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load your CSV
-run = 6
+run = 7
 print(f"run: {run}")
 train = 15
 df = pd.read_csv(f"run_{run}/train/results.csv")
---
+++ run_10.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+# SBATCH directives that convey submission options:
+
+
+#SBATCH --job-name=yolo_finetune
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=jaclin@umich.edu
+#SBATCH --gpus-per-node=1
+#SBATCH --gpus=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-gpu=16
+#SBATCH --mem-per-gpu=64g
+#SBATCH --time=3:30:00
+#SBATCH --account=eecs442s015w25_class
+#SBATCH --partition=gpu
+#SBATCH --output=yolo_run_10.log
+#SBATCH --error=yolo_run_10_%j.err
+
+
+module purge
+module load python/3.12
+source env/bin/activate
+python run_10.py
